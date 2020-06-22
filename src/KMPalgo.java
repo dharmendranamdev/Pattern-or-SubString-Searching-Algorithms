e10091ef7362ff4229c988e34a9144af7543e9ca
Dharmendra Namdev <55890281+dharmendranamdev@users.noreply.github.com>
2020-06-22 23:40:21 +0530

    Update KMPalgo.java

--- src/KMPalgo.java
+++ src/KMPalgo.java
@@ -2,13 +2,13 @@
 public class KMPalgo {
     public static void KMP(String text, int m, String pattern, int n)
     {
-        int pi_table[] = new int[n];//length of pattern String 
-        createPrefix(pattern, n, pi_table);
+        //int pi_table[] = new int[n];//length of pattern String 
+        //createPrefix(pattern, n, pi_table);
         int k = 0;
         for(int i = 0; i < m; i++)
         {
             while( k > 0 && pattern.charAt(k) != text.charAt(i))
-               k = pi_table[k-1];
+               k--;
             if(pattern.charAt(k) == text.charAt(i))
                k += 1;
             if(k == n)
@@ -20,19 +20,7 @@ public class KMPalgo {
         }
         
     }
-    public static void createPrefix(String str, int n,int pi_table[])
-    {
-        int k = 0;
-        pi_table[0] = 0;
-        for(int i = 1; i < n; i++)
-        {
-            while(k > 0 && str.charAt(k+1) != str.charAt(i))
-                k = pi_table[k];
-            if(str.charAt(k+1) == str.charAt(i))
-                k++;
-            pi_table[i] = k;
-        }
-    }
+    
     public static void main(String[] args) {
         String text = "abedabcabc";
         String pattern = "abc";
